53add789910e837527d2d6ca368933a73a4809e2
Rishabh Pathak <clusmsyknight@gmail.com>
2021-03-30 21:30:24 +0530

    renamed app

--- src/containers/UserForm.js
+++ src/containers/UserForm.js
@@ -16,7 +16,7 @@ const UserForm = () => {
   return (
     <div className="container">
       <h1 className="text-center fw-light py-3 shadow-sm mb-4">
-        <span className="fw-bold">Converse</span> A Realtime Messaging App
+        <span className="fw-bold">Clumsy Messenger</span> A Realtime Messaging App
       </h1>
       <ReactCardFlip isFlipped={flipped} flipDirection="vertical">
         <LoginForm handleFlip={handleFlip} />
